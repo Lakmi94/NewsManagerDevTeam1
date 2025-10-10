09be19e7944a0306c4a950c9476f221e24d2b2d5
Lakmi <lakmi94@gmail.com>
2025-10-10 02:54:06 +0200

    add filtering when navigated

--- src/app/category-view/category-view.ts
+++ src/app/category-view/category-view.ts
@@ -1,40 +1,47 @@
-import { Component, OnInit, ElementRef } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { News } from '../services/news';
 import { CommonModule } from '@angular/common';
+import { map, switchMap, startWith, combineLatest } from 'rxjs';
+import { Observable } from 'rxjs';
+
+import { News } from '../services/news';
 import { Article } from '../interfaces/article';
-import * as _ from 'lodash';
 import { SafeHtmlPipe } from '../pipes/safe-html-pipe';
 import { LoginService } from '../services/login-service';
 
 @Component({
   selector: 'app-category-view',
-   standalone: true,
-  imports: [CommonModule, SafeHtmlPipe, RouterLink],
+  standalone: true,
+  imports: [CommonModule, RouterLink, SafeHtmlPipe],
   templateUrl: './category-view.html',
-  styleUrl: './category-view.css',
+  styleUrls: ['./category-view.css'],
 })
-export class CategoryView implements OnInit {
+export class CategoryView {
+  private route = inject(ActivatedRoute);
+  private newsService = inject(News);
+  private loginService = inject(LoginService);
 
-  news: Article[] = [];
-  loggedIn: boolean = false;
-  category: string = 'all';
-  constructor(private route: ActivatedRoute, private newsService: News, public loginService: LoginService) {
+  constructor() {
     this.newsService.setAnonymousApiKey();
   }
-  ngOnInit() {
-    this.route.paramMap.subscribe((params) => {
-      const categoryId = params.get('id');
-      console.log('Category ID:', categoryId);
-      this.category = categoryId ? categoryId : 'all';
-    });
-    this.loginService.user$.subscribe((currentUser) => {
-      this.loggedIn = !!currentUser;
-    });
-    this.newsService.getArticles().subscribe((articles) => {
-      this.news = articles;
-      console.log(this.news);
-      
-    });
-  }
+
+  loggedIn$ = this.loginService.user$.pipe(map((user) => !!user));
+
+  private category$ = this.route.paramMap.pipe(
+    map((params) => (params.get('id')?.toLowerCase() ?? 'all')),
+    startWith('all') // ensures first emission
+  );
+
+  news$: Observable<Article[]> = combineLatest([
+    this.category$,
+    this.newsService.getArticles().pipe(startWith([] as Article[])),
+  ]).pipe(
+    map(([category, articles]) =>
+      category === 'all'
+        ? articles
+        : articles.filter(
+            (a) => a.category?.toLowerCase() === category
+          )
+    )
+  );
 }
