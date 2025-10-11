939808f545fc32cbd426491a76f23139495e55fb
sheila.ghorbanielizeh <sheila.ghorbani@gmail.com>
2025-10-11 16:45:31 -0600

    Adding image functionality

--- src/app/article-edit/article-edit.ts
+++ src/app/article-edit/article-edit.ts
@@ -1,10 +1,11 @@
-import { Component, inject, ViewChild, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Article } from '../interfaces/article';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { News } from '../services/news';
-import { switchMap, take } from 'rxjs/operators';
+import * as _ from 'lodash'; 
+
 
 @Component({
   selector: 'app-article-edit',
@@ -27,6 +28,10 @@ export class ArticleEdit {
     body: '',
   };
 
+  imageError: string | null = null;
+  isImageSaved: boolean = false;
+  cardImageBase64: string | null = null;
+  
   private newsService = inject(News);
   private route = inject(ActivatedRoute);
 
@@ -46,4 +51,41 @@ export class ArticleEdit {
       console.log('Updated:', event);
     });
   }
+  
+  fileChangeEvent(fileInput: any) {
+    this.imageError = null;
+    if (fileInput.target.files && fileInput.target.files[0]) {
+      // Size Filter Bytes
+      const MAX_SIZE = 20971520;
+      const ALLOWED_TYPES = ['image/png', 'image/jpeg'];
+
+      if (fileInput.target.files[0].size > MAX_SIZE) {
+        this.imageError =
+          'Maximum size allowed is ' + MAX_SIZE / 1000 + 'Mb';
+        return false;
+      }
+      if (!_.includes(ALLOWED_TYPES, fileInput.target.files[0].type)) {
+        this.imageError = 'Only Images are allowed ( JPG | PNG )';
+        return false;
+      }
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const image = new Image();
+        image.src = e.target.result;
+        image.onload = rs => {
+          const imgBase64Path = e.target.result;
+          this.cardImageBase64 = imgBase64Path;
+          this.isImageSaved = true;
+
+          this.article.image_media_type = fileInput.target.files[0].type;
+          const head = fileInput.target.files[0].type.length + 13;
+          this.article.image_data = e.target.result.substring(head, e.target.result.length);
+
+        };
+      };
+      reader.readAsDataURL(fileInput.target.files[0]);
+    }
+    return true;
+  }
+
 }
