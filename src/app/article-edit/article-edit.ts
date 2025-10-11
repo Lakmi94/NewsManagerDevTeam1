f07926da4ca601a24821dc8e240629192b6278f0
Lakmi <lakmi94@gmail.com>
2025-10-11 02:57:26 +0200

    fix input issue

--- src/app/article-edit/article-edit.ts
+++ src/app/article-edit/article-edit.ts
@@ -1,66 +1,53 @@
-import { Component, inject, ViewChild} from '@angular/core';
+import { Component, inject, ViewChild, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Article } from '../interfaces/article';
 import { RouterLink, ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { News } from '../services/news';
-import { Observable } from 'rxjs';
-import { take, switchMap } from 'rxjs/operators';
-
-
-
+import { switchMap, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-article-edit',
+  standalone: true,
   imports: [FormsModule, RouterLink, CommonModule],
   templateUrl: './article-edit.html',
-  styleUrl: './article-edit.css'
+  styleUrls: ['./article-edit.css'],
 })
-export class ArticleEdit {
-  article: any = {
-    id: 1,
-    id_user: 1,
-    abstract:"",
-    subtitle:"",
-    update_date:"",
-    category:"",
-    title:"",
-    thumbnail_image: "",
-    thumbnail_media_type:"",
-    body:"",
-  }
+export class ArticleEdit implements OnInit {
+  article: Article = {
+    id: 0,
+    id_user: 0,
+    abstract: '',
+    subtitle: '',
+    update_date: '',
+    category: '',
+    title: '',
+    thumbnail_image: '',
+    thumbnail_media_type: '',
+    body: '',
+  };
+
   private newsService = inject(News);
   private route = inject(ActivatedRoute);
 
-  article$: Observable<Article> = this.route.paramMap.pipe(
-      switchMap(params => {
-        const id = params.get('id');
-        let article = this.newsService.getArticle(id);
-        article.subscribe(event => this.article = event);
-        return article;
-      })
-    );
-  
-  @ViewChild('articleContent') articleContent: any;
-
-  saveArticle(): void {
-    console.log(this.article);
-    this.newsService.updateArticle({...this.article}).subscribe(event => {
-      console.log(event);
-    });
-    this.newsService.getArticle(this.article.id).subscribe(event => {
-      console.log(event);
-    });
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        switchMap((params) => {
+          const id = params.get('id');
+          return this.newsService.getArticle(id!);
+        }),
+        take(1)
+      )
+      .subscribe((article) => {
+        this.article = { ...article };
+      });
   }
 
-      //this.email_service.removeList();
-  // ngOnInit(): void {
-  //   this.route.queryParamMap.subscribe(params => {
-  //     let id = params.get('id'); 
-  //     let num_id: string = String(id);
-  //     let article: Observable<Article> = this.newsService.getArticle(num_id);
-  //     article.pipe(take(1)).subscribe(event => this.article = { event });
-  //   })
-  // }
-  
+  saveArticle(): void {
+    console.log('Saving:', this.article);
+    this.newsService.updateArticle(this.article).subscribe((event) => {
+      console.log('Updated:', event);
+    });
+  }
 }
