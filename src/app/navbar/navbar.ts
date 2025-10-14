08166b0128b661fa5aee82090ab45f28bd05957e
Lakmi <lakmi94@gmail.com>
2025-10-14 14:46:15 +0200

    Fix navbar

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -3,31 +3,36 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { LoginService } from '../services/login-service';
 import { User } from '../interfaces/user';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule, CommonModule, RouterLink,RouterLinkActive],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
+
 export class Navbar {
   user: User = { username: '', password: '' };
   loggedIn: boolean = false;
- constructor(public loginService: LoginService) {}
+  constructor(public loginService: LoginService) { }
 
   @ViewChild('loginForm') loginForm: any;
 
   login(user: User, form: NgForm): void {
-    this.loginService.login(user.username, user.password).subscribe(event => {
-      if (!this.loginService.isLogged()) {
+    this.loginService.login(user.username, user.password).subscribe(success => {
+      if (success) {
+        const name = user.username;
+        form.setValue({ username: name, password: '' });
+      } else {
         window.alert("Username or Password incorrect");
         form.resetForm({ username: '', password: '' });
       }
     });
   }
-  logout(form: NgForm):void{
+
+  logout(form: NgForm): void {
     this.user = { username: '', password: '' };
     this.loginService.logout();
     form.resetForm({ username: '', password: '' });
